20226ca36101e293226a971d8a4fb26539ec9544
GuavaMZ <guava.mzco@gmail.com>
2024-04-23 19:41:47 +0200

    www

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./css/**/*.{html,js,ttf}", "./**/*.html", "./src/**/*.{html,js}"],
+  content: ["./css/**/*.{html,js,ttf}", "./**/*.html", "./src/**/**/*.{html,js}"],
   theme: {
     extend: {},
     fontFamily: {
