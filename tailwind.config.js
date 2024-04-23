84a3464f9eee4fb1952f1616d1d5e740592f077c
GuavaMZ <guava.mzco@gmail.com>
2024-04-23 17:36:11 +0200

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./css/**/*.{html,js,ttf}", "./**/*.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ['Poppins'],
+    },
+  },
+  plugins: [],
+}
