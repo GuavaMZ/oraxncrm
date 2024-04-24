e38db98ec0956ce17c56b21ff6eb5fbcc1096531
GuavaMZ <guava.mzco@gmail.com>
2024-04-25 01:18:37 +0200

    sdsds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./css/**/*.{html,js,ttf}", "./**/*.html", "./src/**/**/*.{html,js}"],
+  content: ["./css/**/*.{html,js,ttf}", "./**/*.{html,js}", "./src/**/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
     fontFamily: {
       poppins: ['Poppins'],
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')({
+      charts: true,
+    }),
+  ],
 }
