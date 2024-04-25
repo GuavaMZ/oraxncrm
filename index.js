7b8f6c463034a8dcdc66e2ee33a174194374632e
GuavaMZ <guava.mzco@gmail.com>
2024-04-25 02:32:16 +0200

    edits

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const options = {
     colors: ["#1A56DB", "#FDBA8C"],
     series: [
         {
-            name: "Organic",
-            color: "#1A56DB",
+            name: "Paid",
+            color: "#2A76F4",
             data: [
                 { x: "Mon", y: 231 },
                 { x: "Tue", y: 122 },
@@ -15,8 +15,34 @@ const options = {
             ],
         },
         {
-            name: "Social media",
-            color: "#FDBA8C",
+            name: "Partially Paid",
+            color: "#1CCAB8",
+            data: [
+                { x: "Mon", y: 232 },
+                { x: "Tue", y: 113 },
+                { x: "Wed", y: 341 },
+                { x: "Thu", y: 224 },
+                { x: "Fri", y: 522 },
+                { x: "Sat", y: 411 },
+                { x: "Sun", y: 243 },
+            ],
+        },
+        {
+            name: "Unpaid",
+            color: "#90AAD8",
+            data: [
+                { x: "Mon", y: 232 },
+                { x: "Tue", y: 113 },
+                { x: "Wed", y: 341 },
+                { x: "Thu", y: 224 },
+                { x: "Fri", y: 522 },
+                { x: "Sat", y: 411 },
+                { x: "Sun", y: 243 },
+            ],
+        },
+        {
+            name: "Overdue",
+            color: "#D9E6FD",
             data: [
                 { x: "Mon", y: 232 },
                 { x: "Tue", y: 113 },
@@ -39,9 +65,9 @@ const options = {
     plotOptions: {
         bar: {
             horizontal: false,
-            columnWidth: "70%",
+            columnWidth: "50%",
             borderRadiusApplication: "end",
-            borderRadius: 8,
+            borderRadius: 5,
         },
     },
     tooltip: {
@@ -61,7 +87,7 @@ const options = {
     },
     stroke: {
         show: true,
-        width: 0,
+        width: 25,
         colors: ["transparent"],
     },
     grid: {
@@ -108,3 +134,71 @@ if (document.getElementById("column-chart") && typeof ApexCharts !== 'undefined'
     chart.render();
 }
 
+
+// Quick Infos Chart
+const getChartOptions = () => {
+    return {
+      series: [90, 85, 70],
+      colors: ["#1C64F2", "#16BDCA", "#FDBA8C"],
+      chart: {
+        height: "380px",
+        width: "100%",
+        type: "radialBar",
+        sparkline: {
+          enabled: true,
+        },
+      },
+      plotOptions: {
+        radialBar: {
+          track: {
+            background: '#E5E7EB',
+          },
+          dataLabels: {
+            show: false,
+          },
+          hollow: {
+            margin: 0,
+            size: "32%",
+          },
+         
+        },
+        borderRadius: 5,
+      },
+      grid: {
+        show: false,
+        strokeDashArray: 4,
+        padding: {
+          left: 2,
+          right: 2,
+          top: -23,
+          bottom: -20,
+        },
+      },
+      labels: ["Done", "In progress", "To do"],
+      legend: {
+        show: true,
+        position: "bottom",
+        fontFamily: "Inter, sans-serif",
+      },
+      tooltip: {
+        enabled: true,
+        x: {
+          show: false,
+        },
+      },
+      yaxis: {
+        show: false,
+        labels: {
+          formatter: function (value) {
+            return value + '%';
+          }
+        }
+      }
+    }
+  }
+  
+  if (document.getElementById("radial-chart") && typeof ApexCharts !== 'undefined') {
+    const chart = new ApexCharts(document.querySelector("#radial-chart"), getChartOptions());
+    chart.render();
+  }
+  
